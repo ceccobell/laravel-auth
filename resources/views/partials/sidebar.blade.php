faaf10d96d302c2dae5b23636f78b32d9d755137
ceccobell <161962926+ceccobell@users.noreply.github.com>
2024-10-08 16:35:07 +0200

    [UPD] added controller and some style

---
+++ resources/views/partials/sidebar.blade.php
@@ -0,0 +1,7 @@
+<div id="sidebar">
+    <ul class="list-unstyled">
+        <li class="p-3">
+            <a href="#" class="text-light">Progetti</a>
+        </li>
+    </ul>
+</div>
--- routes/web.php
+++ routes/web.php
@@ -18,9 +18,15 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/dashboard', function () {
+/* Route::get('/dashboard', function () {
     return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard'); */
+
+Route::middleware(['auth', 'verified'])->prefix('admin')->name('admin.')->group(function(){
+    Route::get('/', function () {
+            return view('dashboard');
+        })->name('dashboard');
+});
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
